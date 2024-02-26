8b3fb7b7a3534757e2c44eaa3b152c85db212376
Srijan Anand Gupta <guptasrijan85@gmail.com>
2024-02-26 22:28:41 +0530

    Basic Calculator

--- helloworld.cpp
+++ helloworld.cpp
@@ -2,12 +2,7 @@
 using namespace std;
 
 int main() {
-    int num1, num2, sum;
-    num1 = 10;
-    num2 = 5;
-    sum = num1 + num2;
-
-    cout << "Sum of " << num1 << " and " << num2 << " is " << sum << endl;
+    
     cout << "Hello World!" << endl;
 
     return 0;
