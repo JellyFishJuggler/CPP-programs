31bdabbf3424957aa28d074096c6394577ed92ac
Srijan Anand Gupta <guptasrijan85@gmail.com>
2024-03-12 00:53:13 +0530

    Power of 4

--- Mar/12 Mar/isPowerOf4.cpp
+++ Mar/12 Mar/isPowerOf4.cpp
@@ -1,14 +1,13 @@
 class Solution {
-    public:
-    bool isPowerOfFour(long long int n){
-        if(n == 0 || n == 2){
-            return false;
-        }
+public:
+    bool isPowerOfFour(long long int n) {
+        if (n == 0 || n == 2) {return false;}
+        if (n == 1) {return true;}
+       
         n = n >> 2;
-        if((n ^ (n-1)) == 0){
+        if (((n ^ (n - 1)) & 1) == 1) {
             return true;
-        }
-        else{
+        } else {
             return false;
         }
     }
