3555aef3b0152f5f3e6464cd01858c81ce17ffd3
Srijan Anand Gupta <146509602+JellyFishJuggler@users.noreply.github.com>
2024-03-22 18:58:57 +0000

    Two Sum

--- Mar/21 Mar/first_last_index.cpp
+++ Mar/21 Mar/first_last_index.cpp
@@ -22,6 +22,28 @@ int searchRange_left(int nums[], int key, int right, int left) {  //Array ke mid
     return ans;
 }
 
+int searchRange_right(int nums[], int key, int right, int left) {  //Array ke mid se right side me last occurence return ho rhi h
+
+    int mid = left + (right - left) / 2;
+    int ans = -1;
+    while( left <= right){
+        if(nums[mid] == key){
+            ans = mid;
+            left = mid + 1;
+        }
+        else if( nums[mid] > key){
+            right = mid - 1;
+        }
+        else if( nums[mid] < key){
+            left = mid + 1;
+        }
+
+        mid = left + (right - left) / 2;
+    }
+
+    return ans;
+}
+
 // ab same function bna dunga for right side ka i.e. last occurence
 // jisme bas start = mid + 1 kardena first if condition me 
 
@@ -30,13 +52,9 @@ int main(){
   int target = 8;
   int s = 0;
   int e = 5;
-  int index = searchRange_left(arr,target,e,s);
-  cout << "1st Occurence Index is " << index;
+  int index_first = searchRange_left(arr,target,e,s);
+  int index_last = searchRange_right(arr,target,e,s);
+  cout << "1st Occurence Index is " << index_first;
+  cout << "Last Occurence Index is " << index_last;
   return 0;
 }
-
-
-/*
-bki mera deemag kaam nhi kr rha h ki ab left aur right wale ko ek hee function me kaise daalunga
-yeh leetcode wale hote he bkl h
-*/
