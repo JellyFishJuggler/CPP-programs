a0dd1abbfd3e512632221c50d9a1aff1c83cdd8a
Srijan Anand Gupta <146509602+JellyFishJuggler@users.noreply.github.com>
2024-04-20 13:09:54 +0000

    Solved

--- Apr/Search_Rotated_Array_II.cpp
+++ Apr/Search_Rotated_Array_II.cpp
@@ -1,41 +1,53 @@
-// TLE aa rha abhi to
-// same tarike se kr rhkha h jaise Part 1 wala kiya tha...
-
 class Solution {
 public:
     bool search(vector<int>& nums, int target) {
             int n = nums.size();
-            int left = 0;
-            int right = n- 1;
-            int mid = left + (right - left) / 2;
-            while(left <= right){
-                    if(nums[mid] == target) {
-                    return true;
-                }
-                if(nums[mid] == nums[left] && nums[mid] == nums[right]){    
-                    left = left + 1;
-                    right = right - 1;
-                    continue;
-                }
-                if(nums[left] <= nums[mid]){
-                    if(nums[left] <= target && nums[mid] >= target){
-                     right = mid - 1;
-                    }
-                    else{
-                        left = mid + 1;
-                    }
-                }
-                else{
-                    if(nums[right] >= target && nums[mid] <= target){
-                        left = mid + 1;
-                    }
-                    else{
-                        right = mid - 1;
-                    }
-                }
+        int start = 0;
+        int end = n-1;
 
+        while(start <= end) {
+
+            int mid = start + (end - start) / 2;
+
+            if(nums[mid] == target || nums[start] == target || nums[end] == target) return true;
+
+            if(nums[start] == nums[end]) {
+
+                ++start;
+                --end;
 
             }
-            return false;
+            else if(nums[mid] >= nums[start]) {
+
+                if(target < nums[mid] && target >= nums[start]) {
+
+                    end = mid - 1;
+
+                }
+                else {
+
+                    start = mid + 1;
+
+                }
+
+            }
+            else {
+
+                if(target > nums[mid] && target <= nums[end]) {
+
+                    start = mid + 1;
+
+                }
+                else {
+
+                    end = mid - 1;
+
+                }
+
+            }
+
+
+        } return false;
+
     }
 };
