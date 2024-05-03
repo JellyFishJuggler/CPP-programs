ecd24e5d93973227d4a9257bcb1a588ad4d7fd05
Srijan Anand Gupta <146509602+JellyFishJuggler@users.noreply.github.com>
2024-05-03 15:39:29 +0000

    Find Pivot

--- Apr/Find_Pivot.cpp
+++ Apr/Find_Pivot.cpp
@@ -19,3 +19,25 @@ int findKRotation(vector<int> &arr){
 
 //TLE aa rha h abhi
 //https://www.naukri.com/code360/problems/rotation_7449070?leftPanelTabValue=PROBLEM - CodingNinjas
+
+
+//Part 2 : find the pivot where the sum of left side is equal to left side of the array.
+
+
+/*
+  int leftSum = 0;
+  int rightSum = 0;
+  
+  for(int i=0;i<n;i++)      ....n is the size of the arrray
+  {
+    rightSum += nums[i];
+  }
+
+  for(int j=0;j<n;j++){
+    rightSum -= nums[j];
+    if(rightSum == leftSum){
+      return j;
+    }
+    leftSum += nums[j];
+  }
+*/
