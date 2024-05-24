c463e647c7e51cc336a987cb0fcbaf19673c0f63
Srijan Anand Gupta <146509602+JellyFishJuggler@users.noreply.github.com>
2024-05-24 16:22:08 +0000

    Kth Missing +ve Number

---
+++ May/kth-missing-positive-number.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int findKthPositive(vector<int>& arr, int k) {
+        int n = arr.size();
+        // Brute Force
+        // for(int i =0; i<n; i++){
+        //     if(arr[i] <= k){
+        //         k++;
+        //     }
+        //     else{
+        //         break;
+        //     }
+        // }
+        // return k;
+
+        //Binary Search
+        
+       // {Waiting List me hai abhi}
+    }
+};
