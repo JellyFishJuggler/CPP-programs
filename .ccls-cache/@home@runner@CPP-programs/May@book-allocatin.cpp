c12a1a965ae78de086cb8041d3a9ab9779fd2fc0
Srijan Anand Gupta <146509602+JellyFishJuggler@users.noreply.github.com>
2024-07-04 13:04:11 +0000

    Upper & Lower Bound

--- .ccls-cache/@home@runner@CPP-programs/May@book-allocatin.cpp
+++ .ccls-cache/@home@runner@CPP-programs/May@book-allocatin.cpp
@@ -1,40 +1,36 @@
-bool isPossible(vector<int>& nums, int k, int n, int mid){
+int countStudents(vector<int> &arr, int pages) {
+    int n = arr.size();
     int students = 1;
-    int pagesCount =  0;
-    for(int i=0; i<n; i++){
-        if(pagesCount + nums[i] <= mid){
-            pagesCount += nums[i];
+    long long pagesStudent = 0;
+    for (int i = 0; i < n; i++) {
+        if (pagesStudent + arr[i] <= pages) {
+            //pages to current student
+            pagesStudent += arr[i];
         }
-        else{
+        else {
+            // pages to next student
             students++;
-            if(students > k || nums[i] > mid){
-                return false;
-            }
-            pagesCount = nums[i];
+            pagesStudent = arr[i];
         }
     }
-    return true;
+    return students;
 }
 
-int BookAllocation(vector<int>& nums, int k) {
-    int left = 1;
-    int n = nums.size();
-    int sum  = 0;
-    for(int i=0; i<n; i++){
-        sum += nums[i];
-    }
-    int right = sum;
-    int ans = 0;
-    while(left <= right){
-        int mid = left + (right - left) / 2;
-        if(isPossible(nums,k,n,mid)){
-            ans = mid;
-            right  = mid - 1;
-        }
-        else{
-            left = mid + 1;
-        }
-    }
+int findPages(vector<int>& arr, int n, int m) {
+    // Write your code here.
+    if (m > n) return -1;
 
-    return ans;
+    int low = *max_element(arr.begin(), arr.end());
+    int high = accumulate(arr.begin(), arr.end(), 0);
+    while (low <= high) {
+        int mid = (low + high) / 2;
+        int students = countStudents(arr, mid);
+        if (students > m) {
+            low = mid + 1;
+        }
+        else {
+            high = mid - 1;
+        }
+    }
+    return low;
 }
