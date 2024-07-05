24e5d4b97fb6b01c037c21c37e9dddabce5a2747
Srijan Anand Gupta <146509602+JellyFishJuggler@users.noreply.github.com>
2024-07-05 14:15:41 +0000

    Count Number of Nice Subarrays

---
+++ July/5/count-number-nice-subarray.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+
+    int srijan(vector<int>& nums, int k){
+        int l = 0, r = 0, maxlen = 0, len = 0;
+        int n = nums.size();
+        int count = 0;
+        while(r<n){
+
+            if(nums[r] % 2 != 0){
+                k--;
+                count = 0;
+            }
+
+            while(k < 0){
+                if (nums[l] % 2 != 0){
+                    k++;
+                }
+                count++;
+
+                l++;
+            }
+            len = r - l + 1;
+            maxlen += len;
+            //maxlen = max(len,maxlen);
+            r++;
+        }
+
+        return maxlen;
+    }
+
+    int numberOfSubarrays(vector<int>& nums, int k) {
+        return srijan(nums,k) - srijan(nums, k-1);
+    }
+};
